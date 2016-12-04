e9dfd35756250ae7de155d12e157dbc5e93f04bd
tophymastery <akadet85@gmail.com>
2016-12-04 22:35:45 +0700

    day 3 - /profile allow only logged in user

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import firebase from 'firebase'
 
 import Home from './components/Home'
 import Profile from './components/Profile'
@@ -16,6 +17,19 @@ const router = new VueRouter({
     { path: '/user/:id', component: User },
     { path: '/signin', component: Signin }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  console.log(to)
+  if (to.matched.some((x) => x.meta.requireAuth)) {
+    if (firebase.auth().currentUser) {
+      next()
+      return
+    }
+    next({ path: '/signin', query: { redirect: to.fullPath } })
+    return
+  }
+  next()
 })
 
 export default router
