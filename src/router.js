4ded208e8c2292f8d82dc943301e38cc71762033
tophymastery <akadet85@gmail.com>
2016-12-04 21:49:40 +0700

    day 3 - init component and routes

--- src/router.js
+++ src/router.js
@@ -1,11 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Home from './components/Home'
+import Profile from './components/Profile'
+import User from './components/User'
+import Signin from './components/Signin'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   mode: 'history',
-  routes: []
+  routes: [
+    { path: '/', component: Home },
+    { path: '/profile', component: Profile, meta: { requireAuth: true } },
+    { path: '/user/:id', component: User },
+    { path: '/signin', component: Signin }
+  ]
 })
 
 export default router
