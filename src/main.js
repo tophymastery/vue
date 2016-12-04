8856f19eb9df9dcc70436dd13a97824244cc369c
tophymastery <akadet85@gmail.com>
2016-12-04 21:41:29 +0700

    day 3 - init firebase config

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import router from './router'
+import firebase from 'firebase'
 import App from './App'
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyAvu6wuyRNmOPlLO_TrPrgsNxRIQUrN9Q4',
+  authDomain: 'vue-for-realworld.firebaseapp.com',
+  databaseURL: 'https://vue-for-realworld.firebaseio.com',
+  storageBucket: 'vue-for-realworld.appspot.com',
+  messagingSenderId: '261588433895'
+})
 
 /* eslint-disable no-new */
 new Vue({
