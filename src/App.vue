baa31880789d2a3dd33c132f6664ccae35e2a9e7
tophymastery <akadet85@gmail.com>
2016-12-04 22:08:57 +0700

    day 3 - User component add state

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="app">
+    <router-link to="/">Home</router-link>
+    <router-link to="/profile">Profile</router-link>
+    <router-link to="/user/1">User 1</router-link>
+    <router-link to="/signin">Sign In</router-link>
     <router-view></router-view>
   </div>
 </template>
--- src/components/User.vue
+++ src/components/User.vue
@@ -1,3 +1,27 @@
 <template>
-  <div>User</div>
+  <div>User {{ id }} <br>
+    <router-link :to="`/user/${id - 1}`">Prev</router-link>
+    <router-link :to="`/user/${id + 1}`">Next</router-link>
+  </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      id: 0
+    }
+  },
+  created () {
+    this.reload()
+  },
+  watch: {
+    $route: 'reload'
+  },
+  methods: {
+    reload () {
+      this.id = +this.$route.params.id
+    }
+  }
+}
+</script>
