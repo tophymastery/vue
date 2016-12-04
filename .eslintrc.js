ffb22bf9dbe6530d7fbe6c5de3ba919e3930305e
tophymastery <akadet85@gmail.com>
2016-12-04 21:23:34 +0700

    day 3 - init new project

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,8 +18,5 @@ module.exports = {
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  },
-  global: {
-    $: true
   }
 }
