d3caea6624fb05ac2d1bd35301cfcbecb4356381
tophymastery <akadet85@gmail.com>
2016-12-04 22:29:56 +0700

    day 3 - firebase get current user

--- src/components/TopMenu.vue
+++ src/components/TopMenu.vue
@@ -4,7 +4,23 @@
       <div class="right menu">
         <router-link to="/profile" class="item" active-class="active">Profile</router-link>
         <router-link to="/signin" class="item" active-class="active">Sign In</router-link>
-        <div class="link item">Sign Out</div>
+        <div v-if="currentUser" class="link item">Sign Out</div>
       </div>
     </div>
 </template>
+
+<script>
+import firebase from 'firebase'
+
+export default {
+  data: () => ({
+    currentUser: null
+  }),
+  created () {
+    firebase.auth().onAuthStateChanged((user) => {
+      console.log(user)
+      this.currentUser = user
+    })
+  }
+}
+</script>
