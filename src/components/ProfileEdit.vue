76ea772428b274c426eac264d4389d5305e54c2c
tophymastery <akadet85@gmail.com>
2016-12-11 15:19:03 +0700

    day 4 - include ProfileForm and fix class name

--- src/components/ProfileEdit.vue
+++ src/components/ProfileEdit.vue
@@ -1,11 +1,17 @@
 <template>
-  <div class="ui segemnt">
+  <div class="ui segment">
     <h3 class="ui header">Edit Profile</h3>
+    <profile-form></profile-form>
   </div>
 </template>
 
 <script>
+  import ProfileForm from './ProfileForm'
+
   export default {
+    components: {
+      ProfileForm
+    }
 
   }
 </script>
