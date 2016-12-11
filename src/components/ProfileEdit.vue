aedacdb8d789c8606c7a681d5db38f5f134b92c8
tophymastery <akadet85@gmail.com>
2016-12-11 16:04:48 +0700

    day 4 - save form to firebase

--- src/components/ProfileEdit.vue
+++ src/components/ProfileEdit.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="ui segment">
     <h3 class="ui header">Edit Profile</h3>
-    <profile-form v-model="profile" @save="save"></profile-form>
+    <profile-form v-model="profile" @save="saveProfile"></profile-form>
     {{ profile }}
   </div>
 </template>
 
 <script>
   import ProfileForm from './ProfileForm'
+  import firebase from 'firebase'
 
   export default {
     components: {
@@ -20,9 +21,13 @@
       }
     }),
     methods: {
-      save () {
+      saveProfile () {
         console.log('save form')
         console.log(this.profile)
+        const userId = firebase.auth().currentUser.uid
+        firebase.database()
+          .ref(`user/${userId}`)
+          .set(this.profile)
       }
     }
 
